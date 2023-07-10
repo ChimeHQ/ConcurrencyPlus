f38505d94371078e08635c4527accf9426020c9b
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-07-10 15:31:31 -0400

    Update MainActor.runUnsafely for Swift 5.9

--- Sources/ConcurrencyPlus/MainActor+Unsafe.swift
+++ Sources/ConcurrencyPlus/MainActor+Unsafe.swift
@@ -6,10 +6,26 @@ public extension MainActor {
 	/// This function exists to work around libraries with incorrect/inconsistent concurrency annotations. You should be **extremely** careful when using it, and only as a last resort.
 	///
 	/// It will crash if run on any non-main thread.
+#if compiler(<5.9)
 	@MainActor(unsafe)
 	static func runUnsafely<T>(_ body: @MainActor () throws -> T) rethrows -> T {
 		dispatchPrecondition(condition: .onQueue(.main))
 
 		return try body()
 	}
+#else
+	@_unavailableFromAsync
+	static func runUnsafely<T>(_ body: @MainActor () throws -> T) rethrows -> T {
+#if swift(>=5.9)
+		if #available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *) {
+			return try MainActor.assumeIsolated(body)
+		}
+#endif
+
+		dispatchPrecondition(condition: .onQueue(.main))
+		return try withoutActuallyEscaping(body) { fn in
+			try unsafeBitCast(fn, to: (() throws -> T).self)()
+		}
+	}
+#endif
 }
